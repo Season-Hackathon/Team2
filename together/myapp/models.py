52901938111131618a63f9e66c54e7297bc97a70
yoons02 <kys5037k@naver.com>
2023-02-23 21:22:15 +0900

    대학페이지 이미지 연결

--- together/myapp/models.py
+++ together/myapp/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.utils.html import mark_safe
 
 # Create your models here.
 class Univ(models.Model):
     id=models.AutoField(primary_key=True)
 
     univ = models.CharField(max_length=50)
-    img = models.ImageField(max_length=500, null=True, blank=True)
+    img = models.ImageField(upload_to="img/%Y/%m/%d", null=True, blank=True)
+
 
 class Answer(models.Model):
     ans = models.CharField(default="", max_length=10, null=False, blank=False)
